fdf2175971ded7a3bcd1574da0f4ab6c8f04c469
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2022-09-23 12:28:59 -0700

    commit tree fab753c3f1a461fa2e9cb0d228767c55d8d991ae

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function returnFirstTwoDrivers(){
+    let firstTwoDrivers = drivers.slice(0,2)
+    return firstTwoDrivers
+}
+
+function returnLastTwoDrivers(){
+    let lastTwoDrivers = drivers.slice(2,4)
+    return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(x){
+    return x*4
+}
